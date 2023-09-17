974d9da4d36d6cc0c3ee5a2fd98973fc0700500f
ivan <slinchenk.ivan@mail.ru>
2023-09-17 17:14:42 +0700

    Additional

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 import pandas as pd
+import os
 def convert_series_to_string(series):
     return ', '.join(map(str, series))
-data = pd.read_excel('./lab_pi_101.xlsx')
-# group_name = input ("Введите номер группы ")
+pathname = './lab_pi_101.xlsx'
+if os.path.isfile(pathname):
+    data = pd.read_excel(pathname)
+else:
+    data = pd.read_excel("https://drive.google.com/uc?export=download&id=1Bo5Oili5dAvWDSzAZXjzgjS71IrmLWun")    
+groups = data["Группа"]
+while(True):
+    group_name = input ("Введите номер группы ")
+    if group_name in groups.values:
+        break
+    print("Данные по группе отсутствуют")
 all_marks = data["Оценка"]
-pi101_marks = data[data["Группа"] == "ПИ101"].shape[0]
-students_count = data[data["Группа"] == "ПИ101"]["Личный номер студента"]
+group_marks = data[groups == group_name].shape[0]
+students_count = data[groups == group_name]["Личный номер студента"]
 unique_student_count = students_count.unique()
 unique_control_level = data["Уровень контроля"].unique()
 unique_years = sorted(data["Год"].unique())
-output_string = (f"В исходном датасете содержалось {all_marks.size} оценок, из них {pi101_marks} оценок относятся к группе ПИ101\n"
-f"В датасете находятся оценки {unique_student_count.size} студентов со следующими личными номерами (по ПИ101): {convert_series_to_string(unique_student_count)}\n"
+output_string = (f"В исходном датасете содержалось {all_marks.size} оценок, из них {group_marks} оценок относятся к группе ПИ101\n"
+f"В датасете находятся оценки {unique_student_count.size} студентов со следующими личными номерами {group_name}: {convert_series_to_string(unique_student_count)}\n"
 f"Используемые формы контроля: {convert_series_to_string(unique_control_level)}\n"
 f"Данные представлены по следующим учебным годам: {convert_series_to_string(sorted(unique_years))}")
 print(output_string)
